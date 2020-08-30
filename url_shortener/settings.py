748c3d8b582cbec8e9040bd5c2ff5f050ae7286b
Harsh Agarwal <harsh29092000@gmail.com>
2020-08-30 19:47:37 +0530

    Changed settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE') == "True"
 
-ALLOWED_HOSTS = ['localhost',]
+ALLOWED_HOSTS = ['localhost', 'myurlshortener1.herokuapp.com']
 
 
 # Application definition

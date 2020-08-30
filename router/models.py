b1adcf66f077a2ea6ecdccc0407b926d3e3b1d23
Harsh Agarwal <harsh29092000@gmail.com>
2020-08-30 16:41:03 +0530

    Improved the UI

--- router/models.py
+++ router/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Route(models.Model):
-    original_url = models.URLField()
-    key = models.TextField()
+    original_url = models.URLField(help_text= "Add the original URL that you want to shorten.")
+    key = models.TextField(unique= True, help_text= "Add any random characters of your choice to shorten it.")
 
     def __str__(self):
         return f"{self.key}"
